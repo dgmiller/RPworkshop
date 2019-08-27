b12d297fa9cfb2e376bb531361b9a8ca738ba2d4
Derek Miller <dgmxm7@gmail.com>
2019-08-27 13:15:28 -0500

    Preliminary experimental design script and csv.

---
+++ survey/doe.R
@@ -0,0 +1,31 @@
+# Script to build conjoint survey using formr
+# this is doe.R
+library(here)
+
+# get the full factorial design
+
+ff <- expand.grid(
+  rank = seq(1,30,1),
+  gradtime = seq(4, 7, 1),
+  stipend = seq(10000,50000,5000),
+  placement = seq(1,30,1)
+)
+
+# Sample from full factorial to populate doe
+N    <- 4500
+rows <- sample(x=seq(nrow(ff)), size=N, replace=T)
+doe  <- ff[rows,]
+
+# Add meta data
+nAltsPerQ <- 3 # Number of alternatives per question
+nQPerResp <- 10 # Number of questions per respondent
+nRowsPerResp   <- nAltsPerQ * nQPerResp 
+nResp          <- nrow(doe) / nRowsPerResp # Expecting 500 respondents
+doe$respID     <- rep(seq(nResp), each=nRowsPerResp)
+doe$qID        <- rep(rep(seq(nQPerResp), each=nAltsPerQ), nResp)
+doe$altID      <- rep(seq(nAltsPerQ), nResp*nQPerResp)
+doe$obsID      <- rep(seq(nResp * nQPerResp), each=nAltsPerQ)
+row.names(doe) <- seq(nrow(doe))
+
+# Save design
+write.csv(doe, here::here('survey', 'doe.csv'), row.names=F)
