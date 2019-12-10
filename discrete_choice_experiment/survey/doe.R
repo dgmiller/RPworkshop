29412946efd01c8a3c03664f475b8a5a63bc9c95
Derek Miller <dgmxm7@gmail.com>
2019-12-10 16:35:12 -0600

    updating survey design

--- discrete_choice_experiment/survey/doe.R
+++ discrete_choice_experiment/survey/doe.R
@@ -5,22 +5,23 @@ library(here)
 # get the full factorial design
 
 ff <- expand.grid(
-  price = seq(300,1000,50),
-  screensize = c(7.9, 9.7, 10.5),
-  memorygb = c(32, 64, 128, 256, 512),
-  color = c('Rose Gold', 'Space Grey', 'Silver')
+  price = c("$329", "$399", "$429", "$459", "$529", "$549", "$559", "$679"),
+  screensize = c('7.9 in', '10.2 in'),
+  memorygb = c("32 GB", "64 GB", "128 GB", "256 GB"),
+  color = c('Rose Gold', 'Space Grey', 'Silver'),
+  cellular = c("WiFi only", "WiFi + Cellular")
 )
 
 # Sample from full factorial to populate doe
-N    <- 4500
+nAltsPerQ <- 4 # Number of alternatives per question
+nQPerResp <- 8 # Number of questions per respondent
+nResp <- 500 # Number of respondents
+nRowsPerResp   <- nAltsPerQ * nQPerResp 
+N    <- nResp * nRowsPerResp
 rows <- sample(x=seq(nrow(ff)), size=N, replace=T)
 doe  <- ff[rows,]
 
 # Add meta data
-nAltsPerQ <- 3 # Number of alternatives per question
-nQPerResp <- 10 # Number of questions per respondent
-nRowsPerResp   <- nAltsPerQ * nQPerResp 
-nResp          <- nrow(doe) / nRowsPerResp # Expecting 500 respondents
 doe$respID     <- rep(seq(nResp), each=nRowsPerResp)
 doe$qID        <- rep(rep(seq(nQPerResp), each=nAltsPerQ), nResp)
 doe$altID      <- rep(seq(nAltsPerQ), nResp*nQPerResp)
